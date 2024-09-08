b4d9da4333c1ab02dbbf17d8d9c5ce04323e4fa9
Gabriel Rodrigues <gdev.ti56@gmail.com>
2024-09-08 19:17:02 -0300

    percorrendo listas

---
+++ python/percorrendo listas.py
@@ -0,0 +1,6 @@
+nome = ['lucas', 'tiago', 'maria', 'ana', 'claudia']
+salario = [2000, 1800, 3000, 2500, 2200]
+
+for i in range(5):
+    print(f'o salario de {nome[i]} é {salario[i]} reais')
+#o i começa na posição 0 e vai ate o 4, dessa forma ele vai imprimir o nome e o salario em cada posição das respectivas listas
